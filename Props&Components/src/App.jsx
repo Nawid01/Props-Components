b653c0fd14cfbf217d955e119317d3e3bddee1a2
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-04-18 12:22:46 +0200

    app.jsx und data.js erstellt

--- Props&Components/src/App.jsx
+++ Props&Components/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Card from "./Components/Card";
+import teamdata from "./Data.js";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      {/* passing the "propname"-prop with the "teamdata" to the ChildComponent "Card.jsx "*/}
+      <Card propname={teamdata} />
+    </div>
+  );
 }
 
 export default App;
---
+++ Props&Components/src/Data.js
@@ -0,0 +1,20 @@
+const data = [
+  {
+    id: 1,
+    name: "Nawid",
+    age: 30,
+    city: "Hamburg",
+    phone: "0175/34569723",
+    role: "Webdeveloper",
+    hobbies: "Motocycles and Travelling",
+  },
+  {
+    id: 2,
+    name: "Nadja",
+    age: 31,
+    city: "Regensburg",
+    phone: "0175/34569723",
+    role: "Webdesigner",
+    hobbies: "Painting",
+  },
+];
